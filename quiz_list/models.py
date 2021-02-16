0df99e21be9286fb3e6b2b760dc7bddf4a9b221a
quanpham0805 <shiroemon95@gmail.com>
2021-02-16 01:13:06 -0500

    added models

--- quiz_list/models.py
+++ quiz_list/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class QuizList(models.Model):
+	user_fk = models.ForeignKey('uauth.CustomUser', on_delete = models.CASCADE)
+	quiz_name = models.CharField(max_length = 50)
+	time_limit = models.PositiveIntegerField()
+	words_count = models.PositiveIntegerField(default = 0)
+	quiz_language = models.CharField(max_length = 2)
+	questions_count = models.PositiveIntegerField(default = 0)
+	best_attempt = models.CharField(default="N/A", max_length = 10)
+
+
+
+class Word(models.Model):
+	quiz_id = models.ForeignKey(QuizList, on_delete = models.CASCADE)
+	m_word = models.CharField(max_length=75)
+	word_definition = models.TextField()
+	part_of_speech = models.CharField(max_length=20)
+	example = models.TextField()
+	synonyms = models.TextField()
